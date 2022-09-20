57291fece37f84f2b8402bb73098b32a7851096f
Faisal-pixel <adamsfaisal2003@gmail.com>
2022-09-21 00:33:27 +0100

    adding cart context and shopping icon, also adding the shop page with different product and also styling the shop page, also adding cart dropdown

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication";
+import Shop from "./routes/shop/shop.component";
+import ProductCard from "./components/product-card/product-card.component";
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}> 
         <Route index element={<Home />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,19 @@
 import { Fragment, useContext } from "react"
 import { Outlet, Link } from "react-router-dom"
+
+
+import CartIcon from "../../components/cart-icon/cart-icon.component"
+import CartDropDown  from "../../components/cart-dropdown/cart-dropdown.component"
+
 import { UserContext } from "../../contexts/user.context"
 import {ReactComponent as CrwnLogo} from "../../assets/crown.svg"
 import "./navigation.styles.scss"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
+import { CartContext } from "../../contexts/cart.context"
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext)
-
+    const { isCartOpen } = useContext(CartContext)
     return (
       <Fragment>
         <div className="navigation">
@@ -20,8 +26,11 @@ const Navigation = () => {
                 {
                   currentUser ? <span className="nav-link" onClick={signOutUser}>SIGN OUT</span> : <Link className="nav-link" to="/auth">SIGN IN</Link>
                 }
+                <CartIcon />
             </div>
-            
+            {
+              isCartOpen && <CartDropDown />
+            }
         </div>
         <Outlet />
       </Fragment>
